41a46f669d7fbe913112a1f273e11641e54a3c9c
Asadbek <114920603+Asadbek1317@users.noreply.github.com>
2024-06-14 15:16:30 +0500

    Update inlinekey.py

--- keyboards/inline/inlinekey.py
+++ keyboards/inline/inlinekey.py
@@ -4,21 +4,21 @@ inlinekey1 = InlineKeyboardMarkup(row_width=2,
 
     inline_keyboard = [
         [
-            InlineKeyboardButton(text='Baxtini topgan yigit', url='https://t.me/kitob_sirlari/5'),
-            InlineKeyboardButton(text="qaytganimda uyda bo'l", url="https://t.me/kitob_sirlari/10"),
-            InlineKeyboardButton(text="Baxtiyor oila", url="https://t.me/kitob_sirlari/11"),
-            InlineKeyboardButton(text="janatga elituvchi amallar", url="https://t.me/kitob_sirlari/9"),
+            InlineKeyboardButton(text='Baxtini topgan yigit', url='https://t.me/Abduqodir0vich/218'),
+            InlineKeyboardButton(text="qaytganimda uyda bo'l", url="https://t.me/Abduqodir0vich/218"),
+            InlineKeyboardButton(text="Baxtiyor oila", url="https://t.me/Abduqodir0vich/218"),
+            InlineKeyboardButton(text="janatga elituvchi amallar", url="https://t.me/Abduqodir0vich/218"),
         ],
         [
-            InlineKeyboardButton(text="Deyl karnegi", url="https://t.me/kitob_sirlari/8"),
-            InlineKeyboardButton(text="Sevgilisiga yetishgan Umar hikoyasi", url="https://t.me/kitob_sirlari/12"),
-            InlineKeyboardButton(text="Abu Bakr Siddiq", url="https://t.me/kitob_sirlari/13"),
-            InlineKeyboardButton(text="Ufq", url="https://t.me/kitob_sirlari/16"),
+            InlineKeyboardButton(text="Deyl karnegi", url="https://t.me/Abduqodir0vich/218"),
+            InlineKeyboardButton(text="Sevgilisiga yetishgan Umar hikoyasi", url="https://t.me/Abduqodir0vich/218"),
+            InlineKeyboardButton(text="Abu Bakr Siddiq", url="https://t.me/Abduqodir0vich/218"),
+            InlineKeyboardButton(text="Ufq", url="https://t.me/Abduqodir0vich/218"),
 
         ],
 
         [
-            InlineKeyboardButton(text=" Va Boshqalar", url="https://t.me/kitob_sirlari/48")
+            InlineKeyboardButton(text=" Va Boshqalar", url="https://t.me/Abduqodir0vich/218")
         ]
     ])
 
